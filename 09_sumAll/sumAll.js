78e7bb2995b57169e89416b39dd46b111da6040c
jsonsDev <183668194+jsonsDev@users.noreply.github.com>
2025-07-31 16:25:23 +0800

    Write code to pass tests and complete exercise.

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,18 @@
-const sumAll = function() {
+const sumAll = function(lowerNumber, upperNumber) {
+    if (lowerNumber < 0) return "ERROR";
+    if (!Number.isInteger(lowerNumber) || !Number.isInteger(upperNumber)) return "ERROR";
+    let sum = 0;
+    const start = Math.min(lowerNumber, upperNumber);
+    const end = Math.max(lowerNumber, upperNumber);
+    
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
 
+    return sum;
 };
+
+sumAll(1, 4) // returns the sum of 1 + 2 + 3 + 4 which is 10
 
 // Do not edit below this line
 module.exports = sumAll;
