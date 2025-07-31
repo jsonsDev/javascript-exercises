474252ec60da5dc8b7ca5d78cb9b475ec112b7b9
jsonsDev <183668194+jsonsDev@users.noreply.github.com>
2025-07-31 20:16:07 +0800

    Write code to convert temperatures that pass tests.

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,8 +1,23 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fromFahrenheit) {
+  let toCelsius = (fromFahrenheit - 32) * 5 / 9;
+  if (toCelsius % 1 !== 0) {
+    return parseFloat(toCelsius.toFixed(1));
+  } else {
+    return toCelsius;
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(fromCelsius) {
+  let toFahrenheit = (fromCelsius * 9 / 5 + 32);
+  if (toFahrenheit % 1 !== 0) {
+    return parseFloat(toFahrenheit.toFixed(1));
+  } else {
+    return toFahrenheit;
+  }
 };
+
+convertToCelsius(32);
+convertToFahrenheit(0);
 
 // Do not edit below this line
 module.exports = {
