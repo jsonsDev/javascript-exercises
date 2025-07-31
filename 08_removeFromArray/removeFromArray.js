56e42569128cf784000e8cc0a38876b96f4604fc
jsonsDev <183668194+jsonsDev@users.noreply.github.com>
2025-07-31 12:47:59 +0800

    Write code to complete exercise and pass tests.

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    return array.filter(x => !args.includes(x))
 };
+
+removeFromArray([1, 2, 3, 4], 3); // should remove 3 and return [1,2,4]
 
 // Do not edit below this line
 module.exports = removeFromArray;
