5136afee844fbb5b844b0fd3535d5a37db6a0f49
jsonsDev <183668194+jsonsDev@users.noreply.github.com>
2025-07-31 10:13:40 +0800

    Write code to pass tests and complete exercise.

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    const arr = str.split('');
+    let reversed = arr.reverse();
+    return reversed.join("");
 };
+
+reverseString('hello there') // returns 'ereht olleh'
 
 // Do not edit below this line
 module.exports = reverseString;
