28cbbb576c572951eeb9f39fff2b63fb65d653ac
jsonsDev <183668194+jsonsDev@users.noreply.github.com>
2025-08-19 12:09:08 +0800

    Update object to check palindromes and pass tests.

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (input) {
+    const alphanumerical = "abcdefghijklmnopqrstuvwxyz0123456789"
 
+    const newString = input
+        .toLowerCase()
+        .split("")
+        .filter((character) => alphanumerical.includes(character))
+        .join("");
+
+    const reversedString = newString.split("").reverse().join("");
+
+    return newString === reversedString;
 };
 
 // Do not edit below this line
