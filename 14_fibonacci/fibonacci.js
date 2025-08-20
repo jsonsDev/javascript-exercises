6a49f16134836e239919d120e3e8508eeb4a6add
jsonsDev <183668194+jsonsDev@users.noreply.github.com>
2025-08-20 10:35:21 +0800

    Write logic to find fibonacci sequence member.

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
 
+    if (n == 0) {
+        return 0;
+    } else if (n < 0) {
+        return "OOPS";
+    }
+
+    const fib = [1, 1];
+    for (let i = 2; i < n; i++) {
+        fib[i] = fib[i - 1] + fib[i - 2];
+    }
+
+    return parseInt(fib.slice(n - 1));    
 };
 
 // Do not edit below this line
